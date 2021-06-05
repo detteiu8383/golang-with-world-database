fdc36585b3aeb79df0f5b8e8177400c3d620572e
detteiu8383 <ysdr83@gmail.com>
2021-06-05 12:50:46 +0900

    サーバーとして利用

--- main.go
+++ main.go
@@ -5,10 +5,12 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
+	"github.com/labstack/echo/v4"
 )
 
 type City struct {
@@ -19,20 +21,34 @@ type City struct {
 	Population  int    `json:"population,omitempty"  db:"Population"`
 }
 
+var (
+	db *sqlx.DB
+)
+
 func main() {
-	db, err := sqlx.Connect("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOSTNAME"), os.Getenv("DB_PORT"), os.Getenv("DB_DATABASE")))
+	_db, err := sqlx.Connect("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOSTNAME"), os.Getenv("DB_PORT"), os.Getenv("DB_DATABASE")))
 	if err != nil {
 		log.Fatalf("Cannot Connect to Database: %s", err)
 	}
+	db = _db
 
-	fmt.Println("Connected!")
+	e := echo.New()
+
+	e.GET("/cities/:cityName", getCityHandler)
+
+	e.Start(":13200")
+}
+
+func getCityHandler(c echo.Context) error {
+	cityName := c.Param("cityName")
+	fmt.Println(cityName)
+
 	var city City
-	cityName := os.Args[1]
-	if err := db.Get(&city, "SELECT * FROM city WHERE Name='"+cityName+"'"); errors.Is(err, sql.ErrNoRows) {
-		log.Printf("no such city Name = %s", cityName)
+	if err := db.Get(&city, "SELECT * FROM city WHERE Name=?", cityName); errors.Is(err, sql.ErrNoRows) {
+		log.Printf("no such city Name=%s", cityName)
 	} else if err != nil {
 		log.Fatalf("DB Error: %s", err)
 	}
 
-	fmt.Printf("%sの人口は%d人です\n", cityName, city.Population)
+	return c.JSON(http.StatusOK, city)
 }
